987a92a89c3533c6f52c1c201376350fa4f8ea2f
Stela Capsa <stela.capsa@gmail.com>
2019-10-21 15:11:04 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def update_array_at_with(array, index, value)
+def update_array_at_with(array, index, value = "X")
   array[index] = value
 end
 def input_to_index(input)
