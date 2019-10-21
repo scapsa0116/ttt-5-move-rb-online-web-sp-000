48e2060bb6b46a67fc4c291fbbc85b8ad675b060
Stela Capsa <stela.capsa@gmail.com>
2019-10-21 15:20:42 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,8 +13,8 @@ def input_to_index(input)
   input.to_i-1
 end
 
-def move(board, index, "X") 
-   board[index] = "X"
+def move(board, index, player"X")
+   board[index] = player"X"
 end
 
 def position_taken?(board, index)
