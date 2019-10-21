a035ffbea518658dad2d1b62f13f1ace1b57b9b2
Stela Capsa <stela.capsa@gmail.com>
2019-10-21 15:25:07 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -18,15 +18,3 @@ def move(board, index, player = "X")
 
 end
 
-def position_taken?(board, index)
-   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false
-   else
-      return true
-   end
-end
-def valid_move?(board, index)
-   if index.between?(0,8) && !position_taken?(board, index)
-      return true
-   end
-end
